17af1a5156ed9c0354ff673301949580d2156deb
ianchoo <ianienchoo@gmail.com>
2015-10-11 20:38:06 +0800

    copied solution files

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// JavaScript to be added
+window.onload = function () {
+  document.getElementById("button-yellow")
+  .addEventListener(
+    'click',
+    function() {
+      var person = prompt("Please enter your email");
+    });
+}
+
+
